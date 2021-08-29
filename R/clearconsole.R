4a0c785a8b6bd8f1eb5583dbce0d8b05696aff81
M <47830375+1moein@users.noreply.github.com>
2021-08-28 23:08:02 -0400

    commit 001

---
+++ R/clearconsole.R
@@ -0,0 +1,25 @@
+#' Clears the Console
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clearconsole <- function(){
+
+  cat("\014")
+  cat("\f")
+
+  # if (as.character(Sys.info()['sysname'])=="Windows"){
+  #   cat("\014") #Sends CTRL+L to the Console
+  #   cat("\f") #Sends CTRL+L to the Console
+  # } else {
+  #   cat("\014") #Sends CTRL+L to the Console
+  # }
+  #
+  # # Remove all variables from memory to start fresh
+  # rm(list=ls())
+  # # If there are plots, delete them to start fresh
+  # if (length(dev.list())!= 0) dev.off()
+
+}
+
---
+++ README.md
@@ -0,0 +1,24 @@
+
+# listentodata
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of listentodata is to provide mktg students with the tools they need to extract insights out of mktg data.
+
+## Installation
+
+You can install the released version of listentodata from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("listentodata")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(listentodata)
+## basic example code
+```
