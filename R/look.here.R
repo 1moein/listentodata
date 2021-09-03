cb98d39d4f457dd9ff6d680ef4818f33a0c27fa0
M <47830375+1moein@users.noreply.github.com>
2021-09-03 00:41:41 -0400

    Commit 009

---
+++ R/look.here.R
@@ -0,0 +1,15 @@
+
+#' Select the folder to  results will be saved
+#'
+#' @export
+#'
+look.here = function() {
+  caption = 'Select the folder where your data file is...'
+  if (exists('utils::choose.dir')) {
+    thisdir = utils::choose.dir(caption = caption) 
+  } else {
+    thisdir = easycsv::choose_dir(caption = caption)
+  }
+  setwd(thisdir)
+}
+
